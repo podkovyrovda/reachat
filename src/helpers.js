1d410929e933744efb4c3108270a9d039ea96310
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2019-05-22 03:13:55 +0300

    delete useless files

--- src/helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  filterObjByKey: (obj, key) => {
-    let filtered;
-    for(let objKey in obj) {
-      if (objKey == key) filtered = (obj[key])
-    }
-    return filtered;
-  }
-};
